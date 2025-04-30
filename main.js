a5453273dc7c933121c313daeed89d78790fa070
ydkim519 <ydkim519@gmail.com>
2025-04-30 09:31:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function markAsRead() {
+  const select = document.getElementById('planSelect');
+  const value = select.value;
+  const list = document.getElementById('completedList');
+  const li = document.createElement('li');
+  li.textContent = value + ' – ' + new Date().toLocaleDateString();
+  list.appendChild(li);
+}
