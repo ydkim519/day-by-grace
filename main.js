0750a7f46a5e7ed850227b85510cb582a71eca09
ydkim519 <ydkim519@gmail.com>
2025-04-30 09:40:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-
 function markAsRead() {
   const select = document.getElementById('planSelect');
   const value = select.value;
   const list = document.getElementById('completedList');
   const li = document.createElement('li');
-  li.textContent = value + ' – ' + new Date().toLocaleDateString();
+  li.textContent = `${value} – ${new Date().toLocaleDateString()}`;
   list.appendChild(li);
 }
